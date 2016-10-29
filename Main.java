23f00185cab0648608c10df69ab44b2c56d49e9a
semorai <jltt888@gmail.com>
2016-10-29 10:33:04 +0300

    Muudatused tunnist

--- Main.java
+++ Main.java
@@ -8,7 +8,7 @@ public class Main extends Application{
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        Mang mang = new Mang ();
+        Mang Mang = new Mang ();
 
     }
 }
--- Mang.java
+++ Mang.java
@@ -9,6 +9,7 @@ import javafx.stage.Stage;
  * Created by Jürgen on 25/10/2016.
  */
 public class Mang extends Application {
+    int punktid = 0;
     @Override
     public void start(Stage primaryStage) throws Exception {
 
@@ -24,31 +25,42 @@ public class Mang extends Application {
 
     VBox left = new VBox();
 
-    left.setPrefSize(60, 60);
+    left.setPrefSize(100, 100);
         left.setTranslateY(35);
         left.setTranslateX(35);
 
         button1 = new Button();
-        button1.setPrefSize(25, 25);
+        button1.setPrefSize(100, 100);
         button1.setText("Level1");
             button1.setOnAction(event -> {
                 int count = 0;
                 count++;
                 System.out.println(count);
+                punktid++;
+                button1.setText("" + punktid);
 
                 }
             );
 
     left.getChildren().addAll(button1);
 
+    VBox setCenter = new VBox();
+
+    setCenter.setPrefSize(60, 60);
+        setCenter.setTranslateY(100);
+        setCenter.setTranslateX(100);
+
+        button2 = new Button();
+        button2.setPrefSize(100, 100);
+        button2.setText("Level2");
 
 
+    setCenter.getChildren().addAll(button2);
 
-
-    //border.setCenter(sisu);
     //border.setRight(sisu);
 
     bPane.setLeft(left);
+    bPane.setCenter(setCenter);
 
 
 
