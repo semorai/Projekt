56d4f51941306271b10ac8566a400a4f3cfe9eee
semorai <jltt888@gmail.com>
2016-12-11 10:14:49 +0200

    Loenduri korraastamine

--- Main.java
+++ Main.java
@@ -12,16 +12,9 @@ public class Main extends Application {
 
         Button button0;
         Button button1;
-        Button button2;
-        Button button3;
-        Button button4;
-        Button button5;
-
 
         BorderPane bPane = new BorderPane();
         Scene scene = new Scene(bPane, 800, 650);
-
-
 
         VBox left = new VBox();
 
@@ -35,59 +28,42 @@ public class Main extends Application {
 
         button1 = new Button();
         button1.setPrefSize(100, 100);
-        button1.setText("Level1");
-
-        button2 = new Button();
-        button2.setPrefSize(100, 100);
-        button2.setText("Level2");
-
-        button3 = new Button();
-        button3.setPrefSize(100, 100);
-        button3.setText("Level3");
-
-        button4 = new Button();
-        button4.setPrefSize(100, 100);
-        button4.setText("Level4");
-
-
-        button5 = new Button();
-        button5.setPrefSize(100, 100);
-        button5.setText("Level5");
-
+        button1.setText("Levelup");
 
         button1.setOnAction(event -> {
                     double count = 0;
                     punktid++;
                     button0.setText("" + punktid);
 
-            if (punktid >= 2) button2.setOnAction(event1 -> {
+            if (punktid >= 2 && punktid < 15) {
                         punktid = punktid +2;
                         button0.setText("" + punktid);
-            });
-            else if (punktid >= 15) button3.setOnAction(event2 -> {
-                        punktid = punktid +5;
-                        button0.setText("" + punktid);
-                        System.out.println(punktid);
-            });
-            else if (punktid >= 50) button4.setOnAction(event3 -> {
-                        punktid = punktid +10;
-                        button0.setText("" + punktid);
-                        System.out.println(punktid);
-            });
-            else if (punktid >= 60) button5.setOnAction(event4 -> {
-                        punktid = punktid +20;
-                        button0.setText("" + punktid);
-                        System.out.println(punktid);
-            });
-
 
             }
+            else if (punktid >= 15 && punktid < 50) {
+                punktid = punktid +5;
+                button0.setText("" + punktid);
+                System.out.println(punktid);
+            }
+            else if (punktid >= 50 && punktid < 70)  {
+                punktid = punktid +10;
+                button0.setText("" + punktid);
+                System.out.println(punktid);
+            }
+            else if (punktid >= 60)  {
+                punktid = punktid +20;
+                button0.setText("" + punktid);
+                System.out.println(punktid);
+            }
 
+            else {
+                System.out.println("Nothing can be added");
+
+            }
+            }
         );
 
-
-
-        left.getChildren().addAll(button1,button2, button3, button4,button5);
+        left.getChildren().addAll(button1);
 
         VBox setCenter = new VBox();
 
